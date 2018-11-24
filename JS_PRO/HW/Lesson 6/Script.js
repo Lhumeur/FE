43f9cc927d62c1829cdd21ae8af656e10be64e4e
Lhumeur <ivan.strekha@gmail.com>
2018-11-25 02:34:38 +0300

    HW 6 update

--- JS_PRO/HW/Lesson 6/Script.js
+++ JS_PRO/HW/Lesson 6/Script.js
@@ -3,13 +3,29 @@
 // a=Math.abs,r=(n,s)=>s.split(' ').reduce((p,c)=>a(c-n)<a(p-n)?c:p)
 //
 // console.log(
-//     r("200", "")
+//     r("200", "3 40 -15 90 197 -40 0 33 -1 5 -6")
 // );
 
 const result = function (number, string) {
-    return string.split(' ').reduce((previousValue, currentItem) => Math.abs(currentItem - number) < Math.abs(previousValue - number) ? currentItem : previousValue);
+// Arrow functions:
+//  return string.split(' ').reduce((previousValue, currentItem) => Math.abs(currentItem - number) < Math.abs(previousValue - number) ? currentItem : previousValue);
+    return string.split(' ').reduce(function(previousValue, currentItem) {
+        return Math.abs(currentItem - number) < Math.abs(previousValue - number) ? currentItem : previousValue;
+    });
 }
 
 console.log(
-    result("29", "3 40 90 197 -40 0 33 -1 5 -6")
+    result("-10", "3 40 -15 90 197 -40 0 33 -1 5 -6")
+);
+console.log(
+    result("-11", "3 40 -15 90 197 -40 0 33 -1 5 -6")
+);
+console.log(
+    result("0", "3 40 -15 90 197 -40 0 33 -1 5 -6")
+);
+console.log(
+    result("90", "3 40 -15 90 197 -40 0 33 -1 5 -6")
+);
+console.log(
+    result("29", "3 40 -15 90 197 -40 0 33 -1 5 -6")
 );
